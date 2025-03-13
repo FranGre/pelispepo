d1569939d80fb9aef08fca689a39d05aa25b5877
fran gregori <frangregori61@gmail.com>
2025-03-13 15:34:58 +0100

    userseeder se agrega is_activated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create(['role_id' => '7527e5d0-5cbb-442c-8cb3-1c676b776b2b', 'name' => 'admin', 'email' => 'admin@gmail.com', 'password' => 'Test@1234']);
-        User::create(['role_id' => 'bb34635f-2d3a-4f70-9aec-5e131be8edf0', 'name' => 'fran', 'email' => 'fran@gmail.com', 'password' => 'Test@1234']);
+        User::create(['role_id' => '7527e5d0-5cbb-442c-8cb3-1c676b776b2b', 'name' => 'admin', 'email' => 'admin@gmail.com', 'password' => 'Test@1234', 'is_activated' => true]);
+        User::create(['role_id' => 'bb34635f-2d3a-4f70-9aec-5e131be8edf0', 'name' => 'fran', 'email' => 'fran@gmail.com', 'password' => 'Test@1234', 'is_activated' => true]);
     }
 }

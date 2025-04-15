689a431ca6df921955a28bf53f90c705f204d9f7
fran gregori <frangregori61@gmail.com>
2025-04-15 11:48:06 +0200

    welcome reemplazado el titulo

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,14 +4,14 @@
     <div class="mx-12 my-10">
         <Link class="flex place-content-end" :href="route('login')">Acceder</Link>
 
-        <H1 text="PelisPepo" />
+        <H1 text="NocheDePelis" />
         <H2 text="Hazte algo para comer y disfruta :)" />
 
         <div class="mockup-browser border-base-300 border w-full mt-12">
             <div class="mockup-browser-toolbar">
-                <div class="input">https://pelispepo.vercel.app</div>
+                <div class="input">https://nochedepelis.xyz</div>
             </div>
-            <div class="grid place-content-center border-t border-base-300 h-80">Hello!</div>
+            <div class="grid place-content-center border-t border-base-300 h-80">Hola!!!</div>
         </div>
     </div>
 </template>
